2412962ca57024ccc59904d1e1e9d00c25be75d7
Leonardo <leonardo.ciaccio@gmail.com>
2022-03-04 12:10:35 +0100

    Aggiungere i file di progetto.

---
+++ CallBack Example/CallBack Example.cs
@@ -0,0 +1,61 @@
+﻿/*  CTRADER GURU
+
+    Homepage    : https://ctrader.guru/
+    Telegram    : https://t.me/ctraderguru
+    Twitter     : https://twitter.com/cTraderGURU/
+    Facebook    : https://www.facebook.com/ctrader.guru/
+    YouTube     : https://www.youtube.com/cTraderGuru
+    GitHub      : https://github.com/ctrader-guru
+    LinkedIn    : https://www.linkedin.com/company/ctrader-guru/
+    Discord     : https://discord.gg/yE5XE3z5Nz
+
+*/
+using System;
+using cAlgo.API;
+using cTraderGuru.CallbackExample;
+
+namespace cAlgo.Robots
+{
+    [Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class CallBackExample : Robot
+    {
+        
+        protected override void OnStart()
+        {
+            
+
+
+        }
+
+        protected override void OnTick()
+        {
+
+            HelloWorld.Show(Print, Stop) ;
+
+        }
+
+        protected override void OnStop()
+        {
+
+
+
+        }
+
+    }
+
+}
+
+namespace cTraderGuru.CallbackExample {
+
+    public static class HelloWorld {
+
+        public static void Show(Action<string> MyPrint, Action MyStop) {
+
+            MyPrint( "Hello World cTrader Guru!" );
+            MyStop();
+        
+        }
+    
+    }
+
+}
